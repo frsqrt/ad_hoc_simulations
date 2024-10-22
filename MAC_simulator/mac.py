9d93bcc1f149c11f7088bc77e5123648e30454b0
maurits <mauritskienhuis@gmailcom>
2024-10-22 11:57:36 +0200

    clean-up visualizer

--- MAC_simulator/mac.py
+++ MAC_simulator/mac.py
@@ -6,21 +6,21 @@ import node as n
 import time
 
 # Parameters
-N = 2 # Number of nodes
-X = 5 # Window size
-Y = 5 # Window size
+N = 2  # Number of nodes
+X = 5  # Window size
+Y = 5  # Window size
 radius = 0.25  # Radius of each circle
 min_distance = 0.5  # Minimum distance between circles
-transceive_range = 5.0 # Distance a node can send and receive messages
-num_of_transmissions_per_node = 1 # Number of transmissions a node will make
-propagation_time = 1 # Measured in units/time (5 means the message travels 5 units per loop iteration)
+transceive_range = 5.0  # Distance a node can send and receive messages
+num_of_transmissions_per_node = 1  # Number of transmissions a node will make
+propagation_time = 1  # Measured in units/time (5 means the message travels 5 units per loop iteration)
 
 nodes = []
 
 # Generate random positions while ensuring the minimum distance
 for i in range(0, N):
     new_node = n.create_new_node(i, radius, transceive_range, X, Y)
-    while(n.can_add_node_without_overlap(new_node, nodes, min_distance) == False):
+    while (n.can_add_node_without_overlap(new_node, nodes, min_distance) == False):
         new_node = n.create_new_node(i, radius, transceive_range, X, Y)
 
     #for i in range(0, num_of_transmissions_per_node):
@@ -34,29 +34,64 @@ nodes[0].send_schedule.append(n.Transmission(0, nodes[0], 7, 0, n.Message("HALLO
 for node in nodes:
     node.add_neighbors(nodes)
 
-# Create a plot
-fig, ax = plt.subplots()
-
-# Set the plot limits
-ax.set_xlim((-10, X+10))
-ax.set_ylim((-10, Y+10))
-ax.set_aspect('equal')  # Ensure the circles are not distorted
-
-# Add grid and labels
-ax.grid(True)
-ax.set_title(f'{N} nodes circles with minimum distance {min_distance}')
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-
 simulation_time = 0
 active_transmissions = []
 
-while(1):
+
+class Visualizer:
+    def __init__(self):
+        self.fig = None
+        self.ax = None
+        self.circle_parameters = {'color': 'red', 'fill': False, 'linestyle': '--'}
+        self.set_up_plot()
+
+    def set_up_plot(self):
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlabel("$x_0$")
+        self.ax.set_ylabel("$x_1$")
+        self.ax.set_title("Template title")
+        self.fig.show()
+
+    def draw_function(self, nodes):
+        x_coords = [node.x_pos for node in nodes]
+        y_coords = [node.y_pos for node in nodes]
+        node_circle_range = zip(zip(x_coords, y_coords), [node.transceive_range for node in nodes])
+        colours = [node.get_color_based_on_state() for node in nodes]
+
+        established_links = [
+            (node.receive_buffer.source.x_pos,
+             node.receive_buffer.source.y_pos,
+             node.x_pos - node.receive_buffer.source.x_pos,
+             node.y_pos - node.receive_buffer.source.y_pos)
+            for node in nodes if node.state == n.NodeState.Receiving]
+
+        self.ax.clear()
+        self.ax.scatter(x_coords, y_coords, clip_on=False, color=colours)
+
+        for link in established_links:
+            self.ax.arrow(*link)
+
+        for config in node_circle_range:
+            self.ax.add_patch(plt.Circle(*config, **self.circle_parameters))
+
+        self.ax.set_xlim((-10, X + 10))
+        self.ax.set_ylim((-10, Y + 10))
+        self.ax.set_aspect('equal')  # Ensure the circles are not distorted
+
+        self.ax.set_title('Current network state')
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+        plt.pause(0.5)
+
+
+vis = Visualizer()
+
+while (1):
     print("simulation_time: ", simulation_time)
 
     # Plot each circle
     for node in nodes:
-        node_color = 'blue'
 
         if node.state == n.NodeState.Idle:
             # Check if node can receive
@@ -67,6 +102,7 @@ while(1):
 
                 # Check whether we can already receive the message
                 transmission_propagation_time = simulation_time - transmission.actual_transmit_time
+
                 if transmission_propagation_time >= n.get_distance_between_nodes(transmission.source, node):
                     node.state = n.NodeState.Receiving
                     node.state_counter = transmission.message.length
@@ -89,26 +125,16 @@ while(1):
         elif node.state == n.NodeState.Receiving:
             # TODO: check for collisions
 
-            # Draw line inbetween sender and receiver
-            plt.arrow(node.receive_buffer.source.x_pos, node.receive_buffer.source.y_pos, node.x_pos - node.receive_buffer.source.x_pos, node.y_pos - node.receive_buffer.source.y_pos)
-
             node.state_counter -= 1
             if node.state_counter == 0:
                 # TODO: if data was received, send ACK, don't go idle
                 node.state = n.NodeState.Idle
+
+                # TODO change removal to only occur when message has existed longer than diameter of network + data transmission length
                 active_transmissions.remove(node.receive_buffer)
 
-        node_color = node.get_color_based_on_state()
-
-        circle = plt.Circle((node.x_pos, node.y_pos), node.radius, color=node_color, fill=True)
-        ax.add_artist(circle)
-
-        # Plot the communication range (range field)
-        range_circle = plt.Circle((node.x_pos, node.y_pos), node.transceive_range, color='red', fill=False, linestyle='--')
-        ax.add_artist(range_circle)
+    # TODO create separate draw function.
+    vis.draw_function(nodes)
 
     simulation_time += 1
 
-    plt.pause(0.5)
-
-plt.show()
