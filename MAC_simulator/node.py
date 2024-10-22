70b3e38344b612abfa0f2bb79aa126d8ce12bd8b
maurits <mauritskienhuis@gmailcom>
2024-10-22 12:36:29 +0200

    implement protocol idea

--- MAC_simulator/node.py
+++ MAC_simulator/node.py
@@ -1,6 +1,32 @@
 from dataclasses import dataclass
 from enum import Enum
 import numpy as np
+from typing import Protocol
+
+
+#
+# class MAC_protocol(Protocol):
+#     def receive_packet(self, simulation_time: int, transmission: "Transmission") -> None:
+#         """
+#         Parse incoming tranmission
+#         Checks if the transmission is relevant for this node.
+#         Schedules reply transmission into the send_schedule
+#         :return: Nothing
+#         """
+#
+#     def send_packet(self) -> "Transmission" | None:
+#         """
+#         Keeps track of the back_off, send_schedule, and buffer.
+#         Returns a transmission if it is time to send or as a reaction to a cts.
+#
+#         :return:
+#         """
+#         ...
+#
+#
+# class ALOHA:
+#     ...
+
 
 class NodeState(Enum):
     Idle = 1
@@ -25,6 +51,7 @@ class Node:
     receive_buffer: object
     send_schedule: list
     state_counter: int
+    # protocol: MAC_protocol
 
     def get_packet_arrival_time(self, sender) -> int:
         return int(get_distance_between_nodes(self, sender))
@@ -53,6 +80,8 @@ class Transmission:
     actual_transmit_time: int
     message: Message
 
+
+
 def get_distance_between_nodes(n1: Node, n2: Node) -> float:
     return np.sqrt((n1.x_pos - n2.x_pos) ** 2 + (n1.y_pos - n2.y_pos) ** 2)
 
@@ -71,7 +100,7 @@ def can_add_node_without_overlap(new_node: Node, node_list: list, min_distance: 
     return True # No overlap and minimum distance maintained
 
 
-def idle_state(simulation_time: int, node: Node, active_transmissions: list[Transmission]):
+def idle_state(simulation_time: int, node: Node, active_transmissions: list[Transmission]) -> None | Transmission:
     # Check if node can receive
     for transmission in active_transmissions:
         # Sanity check that we can receive the transmission
@@ -103,10 +132,12 @@ def idle_state(simulation_time: int, node: Node, active_transmissions: list[Tran
             node.state = NodeState.Sending
             node.state_counter = transmission.message.length
 
+
 def sending_state(node: Node):
     node.state_counter -= 1
     if node.state_counter == 0:
         node.state = NodeState.Idle
+
 
 def receiving_state(simulation_time: int, node: Node, active_transmissions: list[Transmission]):
     # Check for collisions
