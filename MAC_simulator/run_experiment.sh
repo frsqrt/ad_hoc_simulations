ac602fdd2b4a58d995739b26e182c5ecf1ed5fb3
Matthias Franz <matthiasfranz@student.tudelft.nl>
2024-11-11 10:31:26 +0100

    mac experiments

---
+++ MAC_simulator/run_experiment.sh
@@ -0,0 +1,311 @@
+#!/bin/bash
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_5_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_5_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_5_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_5_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_10_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_10_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_10_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_10_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_15_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_15_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_15_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_15_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_20_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_20_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_20_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_20_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_25_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_25_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_25_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_aloha_25_random_max_100
+    echo "Completed iteration $i"
+done
+
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+#--------------------------------------------------------------
+
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_5_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_5_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_5_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_5_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_10_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_10_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_10_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_10_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_15_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_15_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_15_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_15_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_20_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_20_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_20_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_20_random_max_100
+    echo "Completed iteration $i"
+done
+
+#---------------
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_25_random_max_25
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_25_random_max_50
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_25_random_max_75
+    echo "Completed iteration $i"
+done
+
+for i in {1..25}
+do
+    echo "Running mac.py - Iteration $i"
+    python3 mac.py data_sink_rts_cts_25_random_max_100
+    echo "Completed iteration $i"
+done
+
+---------------
+
+
+
+echo "All done."
